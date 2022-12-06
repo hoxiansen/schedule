40fdddcd0c9a8fdc3e10febf0f874fa32374a86b
hoxiansen <hoxiansen@163.com>
2022-12-06 14:58:29 +0800

    demo

---
+++ src/java/Demo.java
@@ -0,0 +1,9 @@
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
