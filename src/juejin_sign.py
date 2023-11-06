0b6d06a8b24ef4a22790caee461261fd7aa58cfc
hoxiansen <hoxiansen@163.com>
2023-11-06 16:31:43 +0800

    bugfix

--- src/juejin_sign.py
+++ src/juejin_sign.py
@@ -9,7 +9,7 @@ def sign():
     url = 'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uuid=7162434001180722719&spider=0&_signature=_02B4Z6wo00101mDk8UQAAIDD669qpZdPk35g4PXAAPvFpb0nU-..a8U.k5YxPnnYtbg8eaq5DJJIjJAjDB03OFkjn2c.kGMPYLS0GDKY7j4off.p3QqaRed5aRcnetb6yO4bynyObTzJeO3p3a'
     cookie = '' if environ['JUEJIN_COOKIE'] is None else environ['JUEJIN_COOKIE']
     headers = {
-        'cookie': environ['JUEJIN_COOKIE'],
+        'cookie': cookie,
         'origin': 'https://juejin.cn',
         'referer': 'https://juejin.cn/',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
